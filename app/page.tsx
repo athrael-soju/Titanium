3c31bbb2745fa6764dc786caff406ccb2b233137
Athrael Soju <athrael.soju@gmail.com>
2023-11-14 22:48:49 +0000

    Create basic mockup template

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,80 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import React, { useState, useEffect, useRef } from 'react';
+import { AppBar, Toolbar, Typography, TextField } from '@mui/material';
+import styles from './page.module.css';
+
+interface IMessage {
+  text: string;
+  sender: 'user' | 'ai';
+}
 
 export default function Home() {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(scrollToBottom, [messages]);
+
+  const handleSendMessage = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === 'Enter') {
+      const target = event.target as HTMLInputElement;
+      const userMessage = target.value;
+      target.value = ''; // Clear the input field
+      if (userMessage.trim()) {
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { text: userMessage, sender: 'user' },
+        ]);
+        // Mock AI response
+        setTimeout(() => {
+          setMessages((prevMessages) => [
+            ...prevMessages,
+            { text: `AI Response to: "${userMessage}"`, sender: 'ai' },
+          ]);
+        }, 500);
+      }
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6">Chat Application</Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={styles.main}>
+        {/* Main window for conversation */}
+        <div className={styles.chatWindow}>
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={
+                message.sender === 'user'
+                  ? styles.userMessage
+                  : styles.aiMessage
+              }
+            >
+              {message.text}
+            </div>
+          ))}
+          {/* Invisible element to auto-scroll to */}
+          <div ref={messagesEndRef} />
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        {/* Input box at the bottom */}
+        <div className={styles.inputBox}>
+          <TextField
+            fullWidth
+            label="Type your message"
+            variant="outlined"
+            onKeyDown={handleSendMessage}
+          />
+        </div>
+      </main>
+    </>
+  );
 }
