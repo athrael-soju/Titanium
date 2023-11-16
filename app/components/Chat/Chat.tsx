6723a266f1c154325d069c90699ed210f50b9fd8
Athrael Soju <athrael.soju@gmail.com>
2023-11-16 22:10:49 +0000

    Refactoring

--- app/components/Chat/Chat.tsx
+++ app/components/Chat/Chat.tsx
@@ -1,8 +1,7 @@
-// components/Chat.tsx
 import React, { useState, useEffect, useRef } from 'react';
 import { TextField } from '@mui/material';
 import { v4 as uuidv4 } from 'uuid';
-import styles from './Chat.module.css'; // Make sure this points to your actual CSS module file for the Chat component.
+import styles from './Chat.module.css';
 
 interface IMessage {
   text: string;
@@ -20,74 +19,103 @@ const Chat = () => {
 
   useEffect(scrollToBottom, [messages]);
 
+  const addUserMessageToState = (message: string) => {
+    const userMessageId = uuidv4();
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { text: `🧑‍💻 ${message}`, sender: 'user', id: userMessageId },
+    ]);
+  };
+
+  const addAiMessageToState = (
+    aiResponseText: string,
+    aiResponseId: string
+  ) => {
+    setMessages((prevMessages) => [
+      ...prevMessages.filter((msg) => msg.id !== aiResponseId),
+      { text: `🤖 ${aiResponseText}`, sender: 'ai', id: aiResponseId },
+    ]);
+  };
+
+  const handleAIResponse = async (userMessage: string) => {
+    try {
+      const response = await fetch('/api/chat', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ userMessage }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      return response.body?.getReader();
+    } catch (error) {
+      console.error('Failed to fetch AI response:', error);
+    }
+  };
+
+  const processAIResponseStream = async (
+    reader: ReadableStreamDefaultReader<Uint8Array> | undefined,
+    aiResponseId: string
+  ) => {
+    if (!reader) {
+      console.error(
+        'No reader available for processing the AI response stream.'
+      );
+      return;
+    }
+
+    const decoder = new TextDecoder();
+    let aiResponseText = '';
+
+    const processText = async ({
+      done,
+      value,
+    }: {
+      done: boolean;
+      value?: Uint8Array;
+    }): Promise<void> => {
+      if (done) {
+        return;
+      }
+
+      const chunk = value ? decoder.decode(value, { stream: true }) : '';
+      const lines = chunk.split('\n');
+
+      lines.forEach((line) => {
+        if (line) {
+          try {
+            const json = JSON.parse(line);
+            if (json?.choices[0].delta.content) {
+              aiResponseText += json.choices[0].delta.content;
+            }
+          } catch (error) {
+            console.error('Failed to parse JSON:', line, error);
+          }
+        }
+      });
+
+      addAiMessageToState(aiResponseText, aiResponseId);
+
+      return reader.read().then(processText);
+    };
+    await reader.read().then(processText);
+  };
+
   const handleSendMessage = async (
     event: React.KeyboardEvent<HTMLDivElement>
   ) => {
     if (event.key === 'Enter') {
       const target = event.target as HTMLInputElement;
-      const userMessage = target.value;
-      if (userMessage.trim()) {
-        const userMessageId = uuidv4();
-        setMessages((prevMessages) => [
-          ...prevMessages,
-          { text: `🧑‍💻 ${userMessage}`, sender: 'user', id: userMessageId },
-        ]);
-
+      const userMessage = target.value.trim();
+      if (userMessage) {
+        addUserMessageToState(userMessage);
         target.value = ''; // Clear the input field
 
-        try {
-          const response = await fetch('/api/chat', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ userMessage }),
-          });
-
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-
-          const reader = response?.body?.getReader();
-          const decoder = new TextDecoder();
-
-          let aiResponseId = uuidv4();
-          let aiResponseText = '';
-
-          reader?.read().then(function processText({ done, value }) {
-            if (done) {
-              return;
-            }
-
-            // Decode the stream chunk and parse each line as JSON
-            const chunk = decoder.decode(value, { stream: true });
-            const lines = chunk.split('\n');
-
-            lines.forEach((line) => {
-              if (line) {
-                try {
-                  const json = JSON.parse(line);
-                  if (json?.choices[0].delta.content) {
-                    aiResponseText += json.choices[0].delta.content;
-                  }
-                } catch (error) {
-                  console.error('Failed to parse JSON:', line, error);
-                }
-              }
-            });
-
-            // Update the messages state with the latest AI response text
-            setMessages((prevMessages) => [
-              ...prevMessages.filter((msg) => msg.id !== aiResponseId),
-              { text: `🤖 ${aiResponseText}`, sender: 'ai', id: aiResponseId },
-            ]);
-
-            // Read the next chunk
-            reader.read().then(processText);
-          });
-        } catch (error) {
-          console.error('Failed to fetch AI response:', error);
-        }
+        const aiResponseId = uuidv4();
+        const reader = await handleAIResponse(userMessage);
+        await processAIResponseStream(reader!, aiResponseId);
       }
     }
   };
