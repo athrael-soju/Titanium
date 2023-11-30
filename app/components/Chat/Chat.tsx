22ed502e9300f0cde62fd2024182f48d3bb835ac
Athrael Soju <athrael.soju@gmail.com>
2023-11-30 18:02:11 +0000

    Remove useMessages hook and refactor message handling

--- app/components/Chat/Chat.tsx
+++ app/components/Chat/Chat.tsx
@@ -1,14 +1,19 @@
 // Chat.tsx
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField } from '@mui/material';
-import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 import MessagesField from './MessagesField';
 import FileUpload from '../FileUpload/FileUpload';
 import AssistantCreationForm from '../Assistant/AssistantCreationForm';
 import Loader from './Loader';
-import useMessages from '../hooks/useMessages'; // Custom hook for managing messages
 import styles from './Chat.module.css';
+
+interface IMessage {
+  text: string;
+  sender: 'user' | 'ai';
+  id: string;
+}
 
 const Chat: React.FC = () => {
   const [assistantName, setAssistantName] = useState<string>('');
@@ -16,9 +21,8 @@ const Chat: React.FC = () => {
   const [assistantId, setAssistantId] = useState<string>('');
   const [isStartEnabled, setIsStartEnabled] = useState<boolean>(false);
   const [hasStarted, setHasStarted] = useState<boolean>(false);
+  const [messages, setMessages] = useState<IMessage[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  const { messages, addUserMessage, addAiMessage } = useMessages();
 
   useEffect(() => {
     setIsStartEnabled(
@@ -46,6 +50,24 @@ const Chat: React.FC = () => {
     }
   };
 
+  const addUserMessageToState = (message: string) => {
+    const userMessageId = uuidv4();
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { text: `🧑‍💻 ${message}`, sender: 'user', id: userMessageId },
+    ]);
+  };
+
+  const addAiMessageToState = (
+    aiResponseText: string,
+    aiResponseId: string
+  ) => {
+    setMessages((prevMessages) => [
+      ...prevMessages.filter((msg) => msg.id !== aiResponseId),
+      { text: `🤖 ${aiResponseText}`, sender: 'ai', id: aiResponseId },
+    ]);
+  };
+
   const handleAIResponse = async (userMessage: string) => {
     try {
       const response = await fetch('/api/chat', {
@@ -62,42 +84,64 @@ const Chat: React.FC = () => {
     }
   };
 
-  const handleSendMessage = useCallback(
-    async (event: React.KeyboardEvent<HTMLDivElement>) => {
-      if (event.key === 'Enter') {
-        const target = event.target as HTMLInputElement;
-        const userMessage = target.value.trim();
-        if (userMessage) {
-          addUserMessage(userMessage);
-          target.value = '';
-          const reader = await handleAIResponse(userMessage);
-          if (reader) {
-            let aiResponseText = '';
-            const processText = async ({
-              done,
-              value,
-            }: {
-              done: boolean;
-              value?: Uint8Array;
-            }): Promise<void> => {
-              if (done) {
-                addAiMessage(aiResponseText, uuidv4());
-                return;
-              }
-              const chunk = value
-                ? new TextDecoder().decode(value, { stream: true })
-                : '';
-              aiResponseText += chunk;
-              return reader.read().then(processText);
-            };
-            await reader.read().then(processText);
+  const processAIResponseStream = async (
+    reader: ReadableStreamDefaultReader<Uint8Array> | undefined,
+    aiResponseId: string
+  ) => {
+    if (!reader) {
+      console.error(
+        'No reader available for processing the AI response stream.'
+      );
+      return;
+    }
+
+    const decoder = new TextDecoder();
+    let aiResponseText = '';
+    const processText = async ({
+      done,
+      value,
+    }: {
+      done: boolean;
+      value?: Uint8Array;
+    }): Promise<void> => {
+      if (done) {
+        return;
+      }
+      const chunk = value ? decoder.decode(value, { stream: true }) : '';
+      const lines = chunk.split('\n');
+      lines.forEach((line) => {
+        if (line) {
+          try {
+            const json = JSON.parse(line);
+            if (json?.choices[0].delta.content) {
+              aiResponseText += json.choices[0].delta.content;
+            }
+          } catch (error) {
+            console.error('Failed to parse JSON:', line, error);
           }
         }
-      }
-    },
-    [addUserMessage, addAiMessage]
-  );
+      });
+      addAiMessageToState(aiResponseText, aiResponseId);
+      return reader.read().then(processText);
+    };
+    await reader.read().then(processText);
+  };
 
+  const handleSendMessage = async (
+    event: React.KeyboardEvent<HTMLDivElement>
+  ) => {
+    if (event.key === 'Enter') {
+      const target = event.target as HTMLInputElement;
+      const userMessage = target.value.trim();
+      if (userMessage) {
+        addUserMessageToState(userMessage);
+        target.value = '';
+        const aiResponseId = uuidv4();
+        const reader = await handleAIResponse(userMessage);
+        await processAIResponseStream(reader, aiResponseId);
+      }
+    }
+  };
   if (!hasStarted) {
     return isLoading ? (
       <Loader />
@@ -112,7 +156,6 @@ const Chat: React.FC = () => {
       />
     );
   }
-
   return (
     <>
       {isLoading && <Loader />}
