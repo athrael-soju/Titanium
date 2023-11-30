9716cc0921a3598410d3ab03f5b78e8b9990918e
Athrael Soju <athrael.soju@gmail.com>
2023-11-30 21:31:20 +0000

    Remove unused file handling code

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { writeFile, rm } from 'fs/promises';
 
 export async function POST(request: NextRequest) {
   const data = await request.formData();
@@ -9,14 +8,6 @@ export async function POST(request: NextRequest) {
     console.log('No file found in the request');
     return NextResponse.json({ success: false });
   }
-
-  const bytes = await file.arrayBuffer();
-  const buffer = Buffer.from(bytes);
-  // const path = `/tmp/${file.name}`;
-  // await writeFile(path, buffer);
-  // await rm(path);
-
-  // console.log(`File written to ${path}`);
 
   return NextResponse.json({ file: file, success: true });
 }
