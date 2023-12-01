d23773c05e42c14fe4eb1057dacab0e80791aaad
Athrael Soju <athrael.soju@gmail.com>
2023-12-01 12:56:03 +0000

    Remove unused imports in CustomizedInputBase component

--- app/components/Chat/CustomizedInputBase.tsx
+++ app/components/Chat/CustomizedInputBase.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, ChangeEvent, KeyboardEvent } from 'react';
+import React, { useState, useRef } from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
