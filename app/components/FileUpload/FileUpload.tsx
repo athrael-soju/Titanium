1a93693e7b180011e7cffdd97f6478cdb4bd5172
Athrael Soju <athrael.soju@gmail.com>
2023-12-01 01:34:57 +0000

    Remove unused useState import

--- app/components/FileUpload/FileUpload.tsx
+++ app/components/FileUpload/FileUpload.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { IconButton } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
