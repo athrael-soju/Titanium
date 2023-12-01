4bb8dfb8102630c9cc198e84a306771c3d154706
vadim.nicolai-EXT <nicolai.vadim@gmail.com>
2023-12-01 21:14:15 +0200

    react-query init.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Providers from "./providers";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Titanium',
-  description: 'The Super Simple Chat Template',
+  title: "Titanium",
+  description: "The Super Simple Chat Template",
 };
 
 export default function RootLayout({
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
