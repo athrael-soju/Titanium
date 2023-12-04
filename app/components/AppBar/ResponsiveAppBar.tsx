c1a15f10f5d1efa4a4ebde7c976ede51c212647a
Athrael Soju <athrael.soju@gmail.com>
2023-12-04 22:37:39 +0100

    Fix typo in AppBar title

--- app/components/AppBar/ResponsiveAppBar.tsx
+++ app/components/AppBar/ResponsiveAppBar.tsx
@@ -71,7 +71,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            Titanium
+            Titaniumg
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@
 import React from 'react';
 import { SessionProvider } from 'next-auth/react';
 
-import ResponsiveAppBar from './components/Header/ResponsiveAppBar'; // Adjust the path as necessary
-import Chat from './components/Chat/Chat'; // Adjust the path as necessary
-import styles from './page.module.css'; // This should contain styles for the main container
+import ResponsiveAppBar from './components/AppBar/ResponsiveAppBar';
+import Chat from './components/Chat/Chat';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <SessionProvider>
       <ResponsiveAppBar />
       <main className={styles.main}>
-        <Chat /> {/* Streaming Chat component */}
+        <Chat />
       </main>
     </SessionProvider>
   );
