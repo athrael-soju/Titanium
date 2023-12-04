36e0b58395cc1dafc266bbcf0cf4a7501b6d8502
Athrael Soju <athrael.soju@gmail.com>
2023-12-04 22:40:18 +0100

    Fix typo in AppBar component

--- app/components/AppBar/ResponsiveAppBar.tsx
+++ app/components/AppBar/ResponsiveAppBar.tsx
@@ -71,7 +71,7 @@ function ResponsiveAppBar() {
               textDecoration: 'none',
             }}
           >
-            Titaniumg
+            Titanium
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
