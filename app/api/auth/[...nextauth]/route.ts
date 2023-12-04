3bf64918fd9d5d9e1225e4c56d2f26831f717eef
Athrael Soju <athrael.soju@gmail.com>
2023-12-04 22:53:44 +0100

    Add GitHub and Google authentication providers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,16 @@
 import NextAuth from 'next-auth/next';
 import type { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 const providers = [
   GitHubProvider({
-    clientId: process.env.GITHUB_ID ?? '',
-    clientSecret: process.env.GITHUB_SECRET ?? '',
+    clientId: process.env.GITHUB_ID as string,
+    clientSecret: process.env.GITHUB_SECRET as string,
+  }),
+  GoogleProvider({
+    clientId: process.env.GOOGLE_ID as string,
+    clientSecret: process.env.GOOGLE_SECRET as string,
   }),
 ];
 
