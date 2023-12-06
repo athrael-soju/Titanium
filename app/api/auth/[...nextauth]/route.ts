0492258afedb653da4a393989fb14460c8df4a66
Athrael Soju <athrael.soju@gmail.com>
2023-12-06 11:55:12 +0100

    Add anonymous user creation with unique names and email

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,41 @@
-import NextAuth from 'next-auth/next';
+import NextAuth, { User } from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import {
+  uniqueNamesGenerator,
+  Config,
+  adjectives,
+  colors,
+  starWars,
+  animals,
+} from 'unique-names-generator';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { randomUUID } from 'crypto';
+
+const createAnonymousUser = (): User => {
+  // generate a random name and email for this anonymous user
+  const customConfig: Config = {
+    dictionaries: [adjectives, colors, animals, starWars],
+    separator: '-',
+    length: 3,
+    style: 'capital',
+  };
+  // handle is simple-red-aardvark
+  const unique_handle: string = uniqueNamesGenerator(customConfig).replaceAll(
+    ' ',
+    ''
+  );
+  // real name is Red Aardvark
+  const unique_realname: string = unique_handle.split('-').slice(1).join(' ');
+  const unique_uuid: string = randomUUID();
+  return {
+    id: unique_uuid,
+    email: `${unique_handle.toLowerCase()}@guest.com`,
+    name: unique_realname,
+    image: '',
+  };
+};
 
 const providers = [
   GitHubProvider({
@@ -11,6 +45,13 @@ const providers = [
   GoogleProvider({
     clientId: process.env.GOOGLE_ID as string,
     clientSecret: process.env.GOOGLE_SECRET as string,
+  }),
+  CredentialsProvider({
+    name: 'a Guest Account',
+    credentials: {},
+    async authorize(credentials, req) {
+      return createAnonymousUser();
+    },
   }),
 ];
 
