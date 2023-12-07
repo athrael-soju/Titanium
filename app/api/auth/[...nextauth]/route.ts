d243bb8650a9a2fa8a773884ac9235c54ba038e8
Athrael Soju <athrael.soju@gmail.com>
2023-12-07 16:26:24 +0100

    Update NextAuth configuration and MongoDB client

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,14 @@
-import NextAuth, { User } from 'next-auth';
+import NextAuth, {
+  Account,
+  Profile,
+  SessionStrategy,
+  Session,
+  User,
+} from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import { JWT } from 'next-auth/jwt';
+import { AdapterUser } from 'next-auth/adapters';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import clientPromise from '../../../lib/client/mongodb';
@@ -13,6 +21,8 @@ import {
 } from 'unique-names-generator';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { randomUUID } from 'crypto';
+import Debug from 'debug';
+const debug = Debug('nextjs:api:auth');
 
 const createAnonymousUser = (): User => {
   // generate a random name and email for this anonymous user
@@ -49,7 +59,6 @@ const providers = [
     name: 'a Guest Account',
     credentials: {},
     async authorize(credentials, req) {
-      console.log('credentials', credentials);
       return createAnonymousUser();
     },
   }),
@@ -58,6 +67,71 @@ const providers = [
 const options: NextAuthOptions = {
   providers,
   adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    async jwt({
+      token,
+      account,
+      profile,
+    }: {
+      token: JWT;
+      account: Account | null;
+      profile?: Profile;
+    }): Promise<JWT> {
+      if (account && account?.expires_at && account?.type === 'oauth') {
+        // at sign-in, persist in the JWT the GitHub account details to enable brokered requests in the future
+        token.access_token = account.access_token;
+        token.expires_at = account.expires_at;
+        token.refresh_token = account.refresh_token;
+        token.refresh_token_expires_in = account.refresh_token_expires_in;
+        token.provider = 'github';
+      }
+      if (!token.provider) token.provider = 'Titanium';
+      return token;
+    },
+    async session({
+      session,
+      token,
+      user,
+    }: {
+      session: Session;
+      token: JWT;
+      user: AdapterUser;
+    }): Promise<Session> {
+      // don't make the token (JWT) contents available to the client session (JWT), but flag that they're server-side
+      if (token.provider) {
+        session.token_provider = token.provider;
+      }
+      return session;
+    },
+  },
+  events: {
+    async signIn({
+      user,
+      account,
+      profile,
+    }: {
+      user: User;
+      account: Account | null;
+      profile?: Profile;
+    }): Promise<void> {
+      debug(
+        `signIn of ${user.name} from ${user?.provider || account?.provider}`
+      );
+    },
+    async signOut({
+      session,
+      token,
+    }: {
+      session: Session;
+      token: JWT;
+    }): Promise<void> {
+      debug(`signOut of ${token.name} from ${token.provider}`);
+    },
+  },
+  session: {
+    // use default, an encrypted JWT (JWE) store in the session cookie
+    strategy: 'jwt' as SessionStrategy,
+  },
 };
 
 const handler = NextAuth(options);
