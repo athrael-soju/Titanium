fd4c979db15a4117bf711fc8699d449560c82a2a
Athrael Soju <athrael.soju@gmail.com>
2023-12-12 21:03:05 +0000

    Remove "Home" page from AppBar

--- app/components/AppBar/ResponsiveAppBar.tsx
+++ app/components/AppBar/ResponsiveAppBar.tsx
@@ -14,7 +14,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import BlurOnRoundedIcon from '@mui/icons-material/BlurOnRounded';
 import styles from './ResponsiveAppBar.module.css';
-const pages = ['Home', 'About'];
+const pages = ['About'];
 const settings = ['Account', 'Logout'];
 
 function ResponsiveAppBar() {
