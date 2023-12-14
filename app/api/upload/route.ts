946b8f431d666870f962e00f4062e056cb818215
Athrael Soju <athrael.soju@gmail.com>
2023-12-14 22:42:54 +0000

    Refactor chat route and file upload route

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,78 +1,75 @@
 import { NextRequest, NextResponse } from 'next/server';
 import clientPromise from '@/app/lib/client/mongodb';
-import fs from 'fs';
+import fs from 'fs/promises';
 import OpenAI from 'openai';
+import { createReadStream } from 'fs';
+import { tmpdir } from 'os';
+import { join } from 'path';
 
 const openai = new OpenAI();
+
+async function processFileUpload(user: IUser, file: File) {
+  const tempFilePath = join(tmpdir(), file.name);
+  const arrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  await fs.writeFile(tempFilePath, buffer);
+
+  try {
+    const fileStream = createReadStream(tempFilePath);
+    const fileResponse = await openai.files.create({
+      file: fileStream,
+      purpose: 'assistants',
+    });
+
+    const addFileToAssistantResponse =
+      await openai.beta.assistants.files.create(user.assistantId as string, {
+        file_id: fileResponse.id,
+      });
+
+    return addFileToAssistantResponse;
+  } finally {
+    await fs.unlink(tempFilePath);
+  }
+}
 
 export async function POST(request: NextRequest) {
   const data = await request.formData();
   const file = data.get('file') as unknown as File;
-  const userEmail = data.get('userEmail');
+  const userEmail = data.get('userEmail') as string;
+
   if (!userEmail) {
-    return NextResponse.json('userEmail header is required', {
-      status: 400,
-    });
+    return NextResponse.json(
+      { message: 'User email is required' },
+      { status: 400 }
+    );
   }
 
   const client = await clientPromise;
   const db = client.db();
-
-  // Retrieve the user from the database
   const usersCollection = db.collection<IUser>('users');
   const user = await usersCollection.findOne({ email: userEmail });
+
   if (!user) {
-    return NextResponse.json('User not found', { status: 404 });
+    return NextResponse.json({ message: 'User not found' }, { status: 404 });
   }
 
   if (user.isAssistantEnabled) {
     try {
-      // Save the file to the filesystem
-      const tempFilePath = `./temp-${file.name}`;
-      const arrayBuffer = await file.arrayBuffer();
-      const buffer = Buffer.from(arrayBuffer);
-      fs.writeFileSync(tempFilePath, buffer);
-
-      // Create a read stream from the saved file
-      const fileStream = fs.createReadStream(tempFilePath);
-
-      // Pass the read stream to openai.files.create
-      const fileResponse = await openai.files.create({
-        file: fileStream,
-        purpose: 'assistants',
-      });
-
-      // Add the file to the assistant
-      const addFileToAssistantResponse =
-        await openai.beta.assistants.files.create(user.assistantId as string, {
-          file_id: fileResponse.id,
-        });
-
-      // Delete the file after uploading
-      fs.unlinkSync(tempFilePath);
-
+      const addFileToAssistantResponse = await processFileUpload(user, file);
       return NextResponse.json(
-        {
-          message: 'File uploaded',
-          file: addFileToAssistantResponse,
-        },
+        { message: 'File uploaded', file: addFileToAssistantResponse },
         { status: 200 }
       );
     } catch (error) {
       console.error('Error processing file:', error);
       return NextResponse.json(
-        {
-          message: 'Error processing file',
-        },
+        { message: 'Error processing file' },
         { status: 500 }
       );
     }
   } else {
-    console.error('File uploads are only available with the assistant enabled');
     return NextResponse.json(
-      {
-        message: 'File uploads are only available with the assistant enabled',
-      },
+      { message: 'Assistant must be enabled to upload files' },
       { status: 400 }
     );
   }
