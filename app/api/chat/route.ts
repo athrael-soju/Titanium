2fb44304a14f23dd47bfa3cc54948dd34ebe43ea
Athrael Soju <athrael.soju@gmail.com>
2023-12-18 10:19:43 +0000

    Refactor chat route and handle assistant messages

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,89 +1,81 @@
 import OpenAI, { ClientOptions } from 'openai';
-import clientPromise from '../../lib/client/mongodb';
+import clientPromise from '@/app/lib/client/mongodb';
 import { NextRequest, NextResponse } from 'next/server';
 
-if (!process.env.OPENAI_API_KEY) {
-  throw new Error('OPENAI_API_KEY is not set');
-}
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const options: ClientOptions = {
+  apiKey: OPENAI_API_KEY,
+};
 
-const options: ClientOptions = { apiKey: process.env.OPENAI_API_KEY };
-const openai = new OpenAI(options);
-
-async function fetchAssistantMessage(
-  threadId: string,
-  assistantId: string,
-  userMessage: string
-) {
-  await openai.beta.threads.messages.create(threadId, {
-    role: 'user',
-    content: userMessage,
-  });
-
-  let run = await openai.beta.threads.runs.create(threadId, {
-    assistant_id: assistantId,
-  });
-  while (run.status !== 'completed') {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    run = await openai.beta.threads.runs.retrieve(threadId, run.id);
-  }
-
-  const messages = await openai.beta.threads.messages.list(threadId);
-  return messages.data.find((message) => message.role === 'assistant');
-}
-
-async function handlePostRequest(req: NextRequest) {
+export async function POST(req: NextRequest) {
   try {
     const { userMessage, userEmail } = await req.json();
+
     const client = await clientPromise;
     const db = client.db();
+
     const usersCollection = db.collection<IUser>('users');
     const user = await usersCollection.findOne({ email: userEmail });
 
-    if (!user) {
-      return NextResponse.json({ message: 'User not found' }, { status: 404 });
-    }
+    const openai = new OpenAI(options);
 
-    if (user.isAssistantEnabled && user.assistantId && user.threadId) {
-      const assistantMessage = await fetchAssistantMessage(
-        user.threadId,
-        user.assistantId,
-        userMessage
+    if (user?.isAssistantEnabled) {
+      const threadId = user.threadId as string;
+      const assistantId = user.assistantId as string;
+
+      await openai.beta.threads.messages.create(threadId, {
+        role: 'user',
+        content: userMessage,
+      });
+
+      let run = await openai.beta.threads.runs.create(threadId, {
+        assistant_id: assistantId,
+      });
+
+      while (run.status !== 'completed') {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        run = await openai.beta.threads.runs.retrieve(threadId, run.id);
+        console.log('Thread Run status:', run.status);
+      }
+
+      const messages = await openai.beta.threads.messages.list(threadId);
+
+      const assistantMessage = messages.data.find(
+        (message) => message.role === 'assistant'
       );
 
-      // Process and respond with the assistant's message
       if (!assistantMessage) {
-        return NextResponse.json(
-          { error: 'No assistant message found' },
-          { status: 404 }
-        );
+        return NextResponse.json({ error: 'No assistant message found' });
       }
 
       const assistantMessageContent = assistantMessage.content.at(0);
-      if (!assistantMessageContent || !('text' in assistantMessageContent)) {
-        return NextResponse.json(
-          { error: 'No valid assistant message content found' },
-          { status: 404 }
-        );
+      if (!assistantMessageContent) {
+        return NextResponse.json({
+          error: 'No assistant message content found',
+        });
       }
-
-      return new Response(assistantMessageContent.text.value);
+      // Check if the message content is of type MessageContentText
+      if ('text' in assistantMessageContent) {
+        const data = assistantMessageContent.text.value;
+        return new Response(data);
+      } else {
+        // Handle the case where the message content is not text (e.g., image file)
+        return NextResponse.json({
+          error: 'Assistant message content is not text',
+        });
+      }
     } else {
       const completion = openai.beta.chat.completions.stream({
         model: process.env.OPENAI_API_MODEL ?? 'gpt-4-1106-preview',
         messages: [{ role: 'user', content: userMessage }],
         stream: true,
       });
+
+      // Construct and return a new Response object
       return new Response(completion.toReadableStream());
     }
   } catch (error: any) {
-    console.error('Error processing request:', error);
-    return NextResponse.json(
-      { message: 'Error processing request', error: error.message },
-      { status: 500 }
-    );
+    // Return an error response
+    return new Response(error);
   }
-}
-
-export async function POST(req: NextRequest) {
-  return await handlePostRequest(req);
 }
